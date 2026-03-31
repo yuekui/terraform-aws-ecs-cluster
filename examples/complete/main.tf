8fce70268d4c492a7ecb34ea57c552056faf88c0
Yuekui <yuekui@users.noreply.github.com>
2026-03-30 18:41:32 -0700

    Update the image_id in example as well

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -61,7 +61,7 @@ EOT
 
 data "aws_ssm_parameter" "ami" {
   count = var.enabled ? 1 : 0
-  name  = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name  = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
 }
 
 module "autoscale_group" {
